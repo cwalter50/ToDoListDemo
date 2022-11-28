1158fde898748998a23e9dd12fbeecaefd371324
Chris Walter <walt50@gmail.com>
2022-11-27 20:37:11 -0500

    Initial Commit

---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by Christopher Walter on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
