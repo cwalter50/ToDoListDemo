026258748a8cd4838d4d67277ce6d95399931796
Chris Walter <walt50@gmail.com>
2022-11-27 20:37:57 -0500

    MVP with UserDefaults

--- ToDoListDemo/ContentView.swift
+++ ToDoListDemo/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  ToDoListDemo
+//  ToDoList
 //
 //  Created by Christopher Walter on 11/27/22.
 //
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var names = ["Tess", "Gorman", "Bobby", "Michael"]
+    
+    @State var showAlert = false
+    
+    @State var newName = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(names, id: \.self) { item in
+                    Text(item)
+                }
+            }
+            .navigationTitle("Names")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("+") {
+                        showAlert.toggle()
+                    }
+                    .font(.title)
+
+                }
+            }
+            .listStyle(.plain)
         }
-        .padding()
+        .alert("Add Name", isPresented: $showAlert, actions: {
+                    TextField("Enter Name", text: $newName)
+                    Button("Add", action: {
+                        addName()
+                    })
+                    Button("Cancel", role: .cancel, action: {})
+                }, message: {
+                    Text("Please enter a new name 😀.")
+                })
+        .onAppear(perform: loadFromUserDefaults)
+        
+    }
+    
+    func addName(){
+        withAnimation {
+            names.append(newName)
+            newName = ""
+        }
+        saveToUserDefaults()
+        
+    }
+    
+    
+    func saveToUserDefaults() {
+        let defaults = UserDefaults.standard
+        defaults.set(names, forKey: "savedNames")
+        
+    }
+    
+    func loadFromUserDefaults() {
+        let defaults = UserDefaults.standard
+        
+        if let foundNames = defaults.array(forKey: "savedNames") as? [String] {
+            names = foundNames
+        }
+        else {
+            names = []
+            saveToUserDefaults()
+        }
     }
 }
 
